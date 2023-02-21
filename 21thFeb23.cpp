feb184d94732f72ccdc979881c9f2860687ef403
amanjain200 <amanjain189200@gmail.com>
2023-02-21 18:34:30 +0530

    day52

--- 21thFeb23.cpp
+++ 21thFeb23.cpp
@@ -1,61 +1,59 @@
 /*
-Count Cyclic Paths
-MediumAccuracy: 63.26%Submissions: 8K+Points: 4
+Fill the Matrix
+MediumAccuracy: 69.0%Submissions: 9K+Points: 4
 Lamp
-You can earn more Geekbits by participating in GFG Weekly Coding Contest  
+Don't Miss Out on the Chance to Work with Leading Companies! Visit the GFG Job Fair Now!  
 
-Given a triangular pyramid with its vertices marked as O, A, B and C and a number N, the task is to find the number of ways such that a person starting from the origin O initially, reaches back to the origin in N steps. In a single step, a person can go to any of its adjacent vertices.
-
-Lightbox
+Given a matrix with dimensions N x M, entirely filled with zeroes except for one position at co-ordinates X and Y containing '1'. Find the minimum number of iterations in which the whole matrix can be filled with ones.
+Note: In one iteration, '1' can be filled in the 4 neighbouring elements of a cell containing '1'.
 
 
 Example 1:
 
 Input:
-N = 1
-Output: 0
-Explanation: The minimum length of
-a cyclic path is 2.
+N = 2, M = 3
+X = 2, Y = 3
+Output: 3 
+
+Explanation: 3 is the minimum possible 
+number of iterations in which the
+matrix can be filled.
 Example 2:
 
 Input:
-N = 2
-Output: 3
-Explanation: The three paths are :
-O-A-O, O-B-O, O-C-O
+N = 1, M = 1
+X = 1, Y = 1 
+Output: 0
+
+Explanation: The whole matrix is 
+already filled with ones.
 
 Your Task:  
-You don't need to read input or print anything. Your task is to complete the function countPaths() which takes an integer N as input parameter and returns the number of possible paths. Since the answer may be big, return it modulo (10^9+7). 
+You don't need to read input or print anything. Your task is to complete the function minIterations() which takes the dimensions of the matrix N and M and the co-ordinates of the initial position of '1' ie X and Y as input parameters and returns the minimum number of iterations required to fill the matrix.
 
 
-Expected Time Complexity: O(N)
+Expected Time Complexity: O(N*M)
 Expected Auxiliary Space: O(1)
 
 
 Constraints:
-1 ≤ N ≤ 106
-
+1 ≤ N, M ≤ 103
+1 ≤ X ≤ N
+1 ≤ Y ≤ M 
 */
-
 #include<iostream>
 using namespace std;
+
+
 //User function Template for C++
 
 class Solution{   
 public:
-    int countPaths(int N){
-         int MOD=1e9+7;
-        if(N==1)return 0;
-        long int p1=0;
-        long int p2=3;
-        if(N==2)return p2;
-        long int ans;
-        for(int i=3;i<=N;i++)
-        {
-            ans=((p2*2)%MOD+(p1*3)%MOD)%MOD;
-            p1=p2;
-            p2=ans;
-        }
-        return ans%MOD; 
+    int minIteration(int N, int M, int x, int y){    
+        int top = x-1;
+        int down = N-x;
+        int left = y-1;
+        int right = M-y;
+        return (max(top+left, max(top+right, max(down+left, down+right))));
     }
 };
