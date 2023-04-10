8028d0130a8fba6a9fa0f6de23e0f5fa08b83139
amanjain200 <amanjain189200@gmail.com>
2023-04-10 11:36:03 +0530

    Day100

---
+++ 10thApr23.cpp
@@ -0,0 +1,91 @@
+/*
+Maximum Intersecting Lines
+MediumAccuracy: 48.28%Submissions: 29+Points: 4
+Lamp
+You can earn more Geekbits by participating in GFG Weekly Coding Contest  
+
+N horizontal line segments are arranged on the X-axis of a 2D plane. The start and end point of each line segment is given in a Nx2 matrix lines[ ][ ]. Your task is to find the maximum number of intersections possible of any vertical line with the given N line segments.
+
+Example 1:
+
+Input:
+N = 4
+lines[][] = {{1,3}, {2,3}, {1,2}, {4,4}}
+Output:
+3
+Explanation:
+A vertical line at X = 2 passes through 
+{1,3}, {2,3}, {1,2}, ie three of the 
+given horizontal lines.
+Example 2:
+
+Input: 
+N = 3
+lines[][] = {{1, 3}, {5, 6}, {3,4}}
+Output:
+2
+Explanation: 
+A vertical line at X = 3 passes through 
+two of the given horizontal lines which 
+is the maximum possible.
+Your Task:
+You dont need to read input or print anything. Complete the function maxIntersections() which takes the 2D Matrix lines[][] and the integer N as input parameters, and returns the maximum intersections possible.
+
+Expected Time Complexity: O(N*log(N))
+Expected Auxiliary Space: O(N)
+
+Constraints:
+1 ≤ N ≤ 105 
+-109 ≤ lines[i][0] ≤ 109
+lines[i][0] ≤ lines[i][1] ≤ 109
+
+*/
+
+#include<iostream>
+#include<map>
+#include<vector>
+using namespace std;
+
+//{ Driver Code Starts
+// Initial Template for C++
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int maxIntersections(vector<vector<int>> lines, int N) {
+        map<int, int> mp;
+        for(auto it : lines){
+            mp[it[0]]++; 
+            mp[it[1] + 1]--;
+        }
+        int ans = 0, sum = 0;
+        for(auto it : mp) {
+            sum += it.second;
+            ans = max(ans, sum);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<vector<int>> mat(N, vector<int>(2));
+        for (int j = 0; j < 2; j++) {
+            for (int i = 0; i < N; i++) {
+                cin >> mat[i][j];
+            }
+        }
+        Solution obj;
+        cout << obj.maxIntersections(mat, N) << endl;
+    }
+}
+// } Driver Code Ends
