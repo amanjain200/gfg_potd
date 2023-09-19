4bd321c972822b02ebfe9f44c0c5d785726feac7
amanjain200 <amanjain189200@gmail.com>
2023-09-19 22:06:28 +0530

    day262

---
+++ 19thSept23.cpp
@@ -0,0 +1,86 @@
+/*
+
+Find first set bit
+EasyAccuracy: 46.89%Submissions: 139K+Points: 2
+Learn Google Cloud with Curated Lab Assignments. Register, Earn Rewards, Get noticed by experts at Google & Land your Dream Job! 
+
+Given an integer N. The task is to return the position of first set bit found from the right side in the binary representation of the number.
+Note: If there is no set bit in the integer N, then return 0 from the function.  
+
+Example 1:
+
+Input: 
+N = 18
+Output: 
+2
+Explanation: 
+Binary representation of 18 is 010010,the first set bit from the right side is at position 2.
+
+Example 2:
+
+Input: 
+N = 12 
+Output: 
+3 
+Explanation: 
+Binary representation of  12 is 1100, the first set bit from the right side is at position 3.
+
+Your Task:
+The task is to complete the function getFirstSetBit() that takes an integer n as a parameter and returns the position of first set bit.
+
+Expected Time Complexity: O(log N).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+0 <= N <= 108
+*/
+/*
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to find position of first set bit in the given number.
+    unsigned int getFirstSetBit(int n)
+    {
+        unsigned int mask = 1;
+        int cnt=0;
+        while(mask<=n){
+            if((n&mask) != 0) return cnt+1;
+            cnt++;
+            mask<<=1;
+        }
+        
+        return 0;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver code
+int main()
+{
+    int t;
+    cin>>t; // testcases
+    while(t--)
+    {
+        int n;
+        cin>>n; //input n
+        Solution ob;
+        printf("%u\n", ob.getFirstSetBit(n)); // function to get answer
+    }
+	return 0;
+}
+
+// } Driver Code Ends
+
+*/
